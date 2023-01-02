7f99d13fe0abcff1acfc7efeaa9decfb1875c825
Patryk-Kosmider <115877789+Patryk-Kosmider@users.noreply.github.com>
2023-01-02 20:42:46 +0100

    Add files via upload

---
+++ zad5.2.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+	cout << "Podaj rozmiar tablicy : " << endl;
+	int x;
+	cin >> x;
+
+	vector <int> tab(x);
+
+	cout << "Podaj elementy: " << endl;
+
+
+	int el;
+
+	for (int i = 0; i < x; i++) {
+		cin >> el;
+		tab[i] = el;
+	}
+
+	int dl = size(tab);
+
+	int pol = dl / 2;
+	int pom;
+
+	cout << "Tablica wejsciowa: " << endl;
+
+	for(int i = 0; i < pol; i++)
+	{
+		cout << tab[i] << setw(5) << tab[i + pol] << endl;
+	}
+
+	for(int i = 0 ; i < pol; i++)
+	{
+		pom = tab[i];
+		tab[i] = tab[i + pol];
+		tab[i + pol] = pom;
+	}
+
+	cout << "Tablica wyjsciowa: " << endl;
+
+	for (int i = 0; i < pol; i++)
+	{
+		cout << tab[i] << setw(5) << tab[i + pol] << endl;
+	}
+
+
+
+
+
+	return 0;
+
+}
